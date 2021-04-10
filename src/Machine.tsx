d5a8d13589ca1f57a1a0388bb883bbb298e0a387
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-04-10 16:57:50 +0900

    Add Machine.tsx

---
+++ src/Machine.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export type Element = JSX.Element;
+export type Stack = Element[];
+
+export const empty = (): Stack => {
+  return [];
+}
+
+export const pushElement = (stack: Stack, newElement: Element): Stack => {
+  return [newElement, ...stack];
+};
+
+export const popElement = (stack: Stack): Stack => {
+  if (stack.length < 1) {
+    throw new Error("pop from empty stack");
+  }
+  return stack.slice(1);
+}
+
+export const createElement = (stack: Stack, tag: string, npop: number): Stack => {
+  const bottom = stack.slice(npop);
+  const top = stack.slice(0, npop);
+  return pushElement(bottom, React.createElement(tag, {}, top));
+};
