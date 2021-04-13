e62781495e9490561a8ce6f34c852ad80086526d
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-04-13 18:26:41 +0900

    Change instruction system
    mi:x -> x
    mi:xyz -> mi:"xyz"
    mrow/3 -> mrow:3

--- src/App.tsx
+++ src/App.tsx
@@ -22,17 +22,20 @@ const interpret = (stack: M.Stack, cmd: string): M.Stack => {
   if (cmd.match(/^-?([0-9]*.)?[0-9]+$/)) {
     return M.pushMn(stack, cmd);
   }
-  if (cmd.match(/^mi:[a-zA-Z]+$/)) {
-    const found = cmd.match(/^mi:(?<name>[a-zA-Z]+)$/);
-    return M.pushMi(stack, found!.groups!.name);
+  if (cmd.match(/^[a-zA-Z]$/)) {
+    return M.pushMi(stack, cmd);
+  }
+  const Mi = cmd.match(/^mi:"(?<variable>[a-zA-Z]+)"$/);
+  if (Mi) {
+    return M.pushMi(stack, Mi.groups!.variable);
   }
   if (cmd.match(/^@[a-z]+="[^"]*"$/)) {
     const found = cmd.match(/^@(?<name>[a-z]+)="(?<value>[^"]*)"$/);
     console.log(found!.groups!);
     return M.addAttribute(stack, found!.groups!.name, found!.groups!.value);
   }
-  if (cmd.match(/^mrow\/[0-9]+$/)) {
-    const found = cmd.match(/^mrow\/(?<arity>[0-9]+)$/);
+  if (cmd.match(/^mrow:[0-9]+$/)) {
+    const found = cmd.match(/^mrow:(?<arity>[0-9]+)$/);
     const arity: number = +(found!.groups!.arity);
     return M.createElement(stack, 'mrow', arity);
   }
