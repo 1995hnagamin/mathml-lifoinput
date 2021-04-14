db3008e68ae81fe6c08b703819ab5ba4fb86ca1b
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-04-14 23:42:03 +0900

    Remove {provided.placeholder}

--- src/components/MathStack.tsx
+++ src/components/MathStack.tsx
@@ -55,7 +55,6 @@ const MathStack: React.FC<Props> = ({ stack, selected, setSelected }: Props) => 
                 </Draggable>
               ))
             }
-            {provided.placeholder}
           </ul>
         )
       }
