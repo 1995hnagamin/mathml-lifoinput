395d019b8dfaec24830147ed4106f7ea29a623c7
Nagamine, Hideaki <1995.hnagamin@gmail.com>
2023-06-29 18:48:35 +0900

    Add Input.svelte

---
+++ src/routes/Input.svelte
@@ -0,0 +1,13 @@
+<script>
+	let input = '';
+
+	function handleSubmit() {
+		alert(input);
+		input = '';
+	}
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<input type="text" bind:value={input} />
+	<button type="submit">Submit</button>
+</form>
