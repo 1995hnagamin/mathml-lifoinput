43fe58d3e05178eaed164661ac09cc14ab39ab0e
Nagamine, Hideaki <1995.hnagamin@gmail.com>
2023-06-30 00:01:33 +0900

    Update MathStack.svelte and change appearance

--- src/routes/MathStack.svelte
+++ src/routes/MathStack.svelte
@@ -7,9 +7,23 @@
 <ul>
 	{#each items as item (item.id)}
 		<li>
-			<math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+			<math xmlns="http://www.w3.org/1998/Math/MathML">
 				<MathML elem={item.elem} />
 			</math>
 		</li>
 	{/each}
 </ul>
+
+<style>
+	ul {
+		margin-left: 8px;
+		padding: 0;
+	}
+
+	ul li {
+		list-style-type: none;
+		border-left: medium solid #000;
+		margin: 8px 8px 0 0;
+		padding: 0 0 0 8px;
+	}
+</style>
