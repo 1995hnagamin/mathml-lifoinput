692ed5ef7b14f927d25837d6b27410ee1d41d9c1
Nagamine, Hideaki <1995.hnagamin@gmail.com>
2023-06-29 18:37:35 +0900

    Rename .tsx files

---
+++ src/lib/node.ts
@@ -0,0 +1,24 @@
+export type Elem = {
+	tag: string;
+	content: string | Elem | Elem[];
+	attrs: Record<string, string>;
+};
+
+export const createElem = (tag: string, content: string | Elem | Elem[]): Elem => {
+	return {
+		tag: tag,
+		content: content,
+		attrs: {}
+	};
+};
+
+export const addAttr = (elem: Elem, key: string, value: string): Elem => {
+	return {
+		tag: elem.tag,
+		content: elem.content,
+		attrs: {
+			...elem.attrs,
+			[key]: value
+		}
+	};
+};
