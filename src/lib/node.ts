ccf6b93ed6dde347b977b705f9bdb2252d2e0982
Nagamine, Hideaki <1995.hnagamin@gmail.com>
2023-06-30 02:06:46 +0900

    Show MathML source on div.xml-view

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -22,3 +22,28 @@ export const addAttr = (elem: Elem, key: string, value: string): Elem => {
 		}
 	};
 };
+
+export const output = (stream: string[], indent: number, elem: Elem): void => {
+	stream.push(' '.repeat(indent * 2));
+	stream.push('<', elem.tag);
+	for (const [key, value] of Object.entries(elem.attrs)) {
+		stream.push(' ', key, '="', value, '"');
+	}
+	stream.push('>');
+	if (typeof elem.content === 'string') {
+		stream.push(elem.content);
+	} else {
+		for (const child of elem.content) {
+			stream.push('\n');
+			output(stream, indent + 1, child);
+		}
+		stream.push('\n', ' '.repeat(indent * 2));
+	}
+	stream.push('</', elem.tag, '>');
+};
+
+export const stringify = (elem: Elem): string => {
+	const stream: string[] = [];
+	output(stream, 0, elem);
+	return stream.join('');
+};
--- src/routes/XMLView.svelte
+++ src/routes/XMLView.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import type { Elem } from '$lib/node';
+	import { stringify } from '$lib/node';
 	export let elem: Elem | null = null;
 </script>
 
-<div>{elem?.tag}</div>
+<div class="xml-view">
+	<pre>{elem != null ? stringify(elem) : ''}</pre>
+</div>
+
+<style>
+	div.xml-view {
+		white-space: pre-wrap;
+		font-family: monospace;
+	}
+</style>
